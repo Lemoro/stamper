c1fed11ee93a098a62c1e5bb490a7176c4230cac
Lemoro <example@test.tmp>
2023-09-02 13:42:39 +0300

    fix

--- src/Http/Controllers/StamperMainController.php
+++ src/Http/Controllers/StamperMainController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class PageStamperController extends Controller
+class StamperMainController extends Controller
 {
 
 
--- src/routes/stamper.php
+++ src/routes/stamper.php
@@ -4,4 +4,4 @@
 // use Illuminate\Support\Facades\Route;
 
 
-  Route::get('/example/stamper', [\Stamper\Http\Controllers\PageStamperController::class, 'index']);
+  Route::get('/example/stamper', [\Stamper\Http\Controllers\StamperMainController::class, 'index']);
